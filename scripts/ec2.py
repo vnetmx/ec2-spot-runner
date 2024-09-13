23a3fb88bec72c60d7061bbb6eb931b825c68b43
Alejandro Mendez <alejandro.mendez@axgot.com.mx>
2024-09-12 20:18:04 -0600

    fix runner install

--- scripts/ec2.py
+++ scripts/ec2.py
@@ -44,8 +44,9 @@ shutdown -h now
 EOF
 chmod +x $CURRENT_PATH/shutdown_now_script.sh
 
+# Runner agent install
+mkdir -p actions-runner && cd actions-runner
 #export ACTIONS_RUNNER_HOOK_JOB_COMPLETED=$CURRENT_PATH/shutdown_script.sh
-#mkdir -p actions-runner && cd actions-runner
 #echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED=$CURRENT_PATH/shutdown_script.sh" > .env
 
 GH_RUNNER_VERSION={github_action_runner_version}
